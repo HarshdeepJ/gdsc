17ccfba1e0ad8810a3673ec1832dc145ab1c9eb0
HarshdeepJ <111578177+HarshdeepJ@users.noreply.github.com>
2025-04-03 18:28:33 +0530

    debug 3

--- RAG_Model/RAG_copy.py
+++ RAG_Model/RAG_copy.py
@@ -72,10 +72,10 @@ def retrieve_chapter_topic(chapter, topic):
 
 def teach_topic_with_quiz(chapter, topic, year):
     llm = ChatGroq(model="llama3-8b-8192", api_key = st.secrets.REST.GROQ_API_KEY)
-    st.write('In teach_topic_with_quiz')
+    #st.write('In teach_topic_with_quiz')
     docs = retrieve_chapter_topic(chapter, topic)
-    print(type(docs[0]))
-    print(docs)
+    st.write(type(docs[0]))
+    st.write(docs)
     source_text = "\n".join([doc.page_content for doc in docs])
 
     teaching_prompt = f"""
